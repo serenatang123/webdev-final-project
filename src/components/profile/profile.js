5582bcd64e1a40e383d9d1bc9a0ec25450eb9c9f
jiayao0314 <li.jiayao@northeastern.edu>
2021-04-12 15:53:17 -0700

    make it better

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import {connect} from "react-redux";
 import ProfileBio from "./profile-bio";
 import ProfileAbout from "./profile-about";
 import ProfileFavorite from "./profile-favorite";
