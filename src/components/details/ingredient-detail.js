0adf2b174e1f082ce1fda43d5b82f6619fc2ddbc
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-04-13 21:50:22 -0700

    add loop to display ingredients dynamically

--- src/components/details/ingredient-detail.js
+++ src/components/details/ingredient-detail.js
@@ -11,12 +11,22 @@ const IngredientDetail = ({ingredient}) => {
     const findRecipeByRecipeID = () => {
         recipeService.findRecipeByRecipeID(recipeID)
             .then((data) => {
-                // for(var i =0; i<len(data); i++) {
-                //     arr.append[{'in': ,'me'}]
-                // }
                 setRecipe(data)
             })
     }
+
+    const items = [];
+    for (let i = 1; i <= 20; i++) {
+        if (recipe.meals && recipe.meals[0] && recipe.meals[0][`strIngredient${i}`] == "") {
+            break;
+        }
+        items.push(
+            <tr>
+                <td>{recipe.meals && recipe.meals[0] && recipe.meals[0][`strIngredient${i}`]}</td>
+                <td>{recipe.meals && recipe.meals[0] && recipe.meals[0][`strMeasure${i}`]}</td>
+            </tr>)
+    }
+
     return(
 
         <div>
@@ -30,30 +40,30 @@ const IngredientDetail = ({ingredient}) => {
                     <th scope="col">Ingredient</th>
                     <th scope="col">Amount</th>
                 </tr>
+
                 </thead>
                 <tbody>
-
-
-                <tr>
-                    <td scope="row">{recipe.meals && recipe.meals[0] && recipe.meals[0].strIngredient1}</td>
-                    <td>{recipe.meals && recipe.meals[0] && recipe.meals[0].strMeasure1}</td>
-                </tr>
-                <tr>
-                    <td scope="row">{recipe.meals && recipe.meals[0] && recipe.meals[0].strIngredient2}</td>
-                    <td>{recipe.meals && recipe.meals[0] && recipe.meals[0].strMeasure2}</td>
-                </tr>
-                <tr>
-                    <td scope="row">{recipe.meals && recipe.meals[0] && recipe.meals[0].strIngredient3}</td>
-                    <td colSpan="2">{recipe.meals && recipe.meals[0] && recipe.meals[0].strMeasure3}</td>
-                </tr>
-                <tr>
-                    <td scope="row">{recipe.meals && recipe.meals[0] && recipe.meals[0].strIngredient4}</td>
-                    <td colSpan="2">{recipe.meals && recipe.meals[0] && recipe.meals[0].strMeasure4}</td>
-                </tr>
-                <tr>
-                    <td scope="row">{recipe.meals && recipe.meals[0] && recipe.meals[0].strIngredient5}</td>
-                    <td colSpan="2">{recipe.meals && recipe.meals[0] && recipe.meals[0].strMeasure5}</td>
-                </tr>
+                    {items}
+                {/*<tr>*/}
+                {/*    <td scope="row">{recipe.meals && recipe.meals[0] && recipe.meals[0].strIngredient1}</td>*/}
+                {/*    <td>{recipe.meals && recipe.meals[0] && recipe.meals[0].strMeasure1}</td>*/}
+                {/*</tr>*/}
+                {/*<tr>*/}
+                {/*    <td scope="row">{recipe.meals && recipe.meals[0] && recipe.meals[0].strIngredient2}</td>*/}
+                {/*    <td>{recipe.meals && recipe.meals[0] && recipe.meals[0].strMeasure2}</td>*/}
+                {/*</tr>*/}
+                {/*<tr>*/}
+                {/*    <td scope="row">{recipe.meals && recipe.meals[0] && recipe.meals[0].strIngredient3}</td>*/}
+                {/*    <td colSpan="2">{recipe.meals && recipe.meals[0] && recipe.meals[0].strMeasure3}</td>*/}
+                {/*</tr>*/}
+                {/*<tr>*/}
+                {/*    <td scope="row">{recipe.meals && recipe.meals[0] && recipe.meals[0].strIngredient4}</td>*/}
+                {/*    <td colSpan="2">{recipe.meals && recipe.meals[0] && recipe.meals[0].strMeasure4}</td>*/}
+                {/*</tr>*/}
+                {/*<tr>*/}
+                {/*    <td scope="row">{recipe.meals && recipe.meals[0] && recipe.meals[0].strIngredient5}</td>*/}
+                {/*    <td colSpan="2">{recipe.meals && recipe.meals[0] && recipe.meals[0].strMeasure5}</td>*/}
+                {/*</tr>*/}
                 </tbody>
             </table>
         </div>
